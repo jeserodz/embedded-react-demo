adb581ac05221df65cc4f1a17313f9edb44f14da
Jesé Rodríguez <jese200@gmail.com>
2018-04-22 20:54:22 -0400

    implement load.js mechanism

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('irobot-embed-experience'));
-registerServiceWorker();
+// registerServiceWorker();
